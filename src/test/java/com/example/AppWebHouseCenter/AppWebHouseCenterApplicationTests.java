5dcb3e086ecd3a6fefdc9de95510c8c1f6cdfe31
CamiloSilva21 <juancamilosilva1@gmail.com>
2022-08-27 12:42:56 -0500

    Inicialización del proyecto

---
+++ src/test/java/com/example/AppWebHouseCenter/AppWebHouseCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppWebHouseCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebHouseCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
