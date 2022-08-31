d9907093eb50ea48a0b1408341a8568a87ee2b49
CamiloSilva21 <juancamilosilva1@gmail.com>
2022-08-30 21:38:48 -0500

    Dependencia para Postgres añadida y prueba controller empresa

--- src/main/java/com/example/AppWebHouseCenter/controllers/EmpresaController.java
+++ src/main/java/com/example/AppWebHouseCenter/controllers/EmpresaController.java
@@ -1,4 +1,21 @@
 package com.example.AppWebHouseCenter.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmpresaController {
+
+    @GetMapping("/enterprises")
+    public String getEmpresa(){
+        return null;
+    }
+
+    //Pendiente configuración del controller de tipo POST
+    @PostMapping("/enterprises")
+    public String postEmpresa(){
+        return null;
+    }
+
 }
