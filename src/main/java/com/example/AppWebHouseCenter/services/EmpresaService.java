94f4be22c71399544ab5d507d98fce9dd9f41079
CamiloSilva21 <juancamilosilva1@gmail.com>
2022-09-02 15:31:10 -0500

    Actualización Servicio empresa y controlador empresa

--- src/main/java/com/example/AppWebHouseCenter/services/EmpresaService.java
+++ src/main/java/com/example/AppWebHouseCenter/services/EmpresaService.java
@@ -5,11 +5,10 @@ import com.example.AppWebHouseCenter.entities.Empresa;
 import java.util.List;
 
 public interface EmpresaService {
-    public List<Empresa> listarEmpresas(); //lista todas las empresar
-    public Empresa consultarEmpresaPorId(double nitEmpresa); //permite consultar una empresa por su NIT
+    public List<Empresa> listarEmpresas(); //lista todas las empresas
     public Empresa crearEmpresa(Empresa empresa); //Crear una empresa nueva
+    public Empresa consultarEmpresaPorId(Double nitEmpresa); //permite consultar una empresa por su NIT
     public Empresa editarEmpresa(Empresa empresa); // modifica una empresa existente
-
     public void eliminarEmpresa(double nitEmpresa); // borra empresa
 
 
--- src/main/java/com/example/AppWebHouseCenter/services/ImpEmpresaService.java
+++ src/main/java/com/example/AppWebHouseCenter/services/ImpEmpresaService.java
@@ -1,6 +1,8 @@
 package com.example.AppWebHouseCenter.services;
 
 import com.example.AppWebHouseCenter.entities.Empresa;
+import com.example.AppWebHouseCenter.repositories.RepositoryEmpresa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,28 +10,36 @@ import java.util.List;
 @Service
 public class ImpEmpresaService implements EmpresaService {
 
+    @Autowired
+    private RepositoryEmpresa repositoryEmpresa;
+
     @Override
     public List<Empresa> listarEmpresas() {
-        return null;
+
+        return repositoryEmpresa.findAll();
     }
 
     @Override
-    public Empresa consultarEmpresaPorId(double nitEmpresa) {
-        return null;
+    public Empresa consultarEmpresaPorId(Double nitEmpresa) {
+
+        return repositoryEmpresa.findById(nitEmpresa).get();
     }
 
     @Override
     public Empresa crearEmpresa(Empresa empresa) {
-        return null;
+
+        return repositoryEmpresa.save(empresa);
     }
 
     @Override
     public Empresa editarEmpresa(Empresa empresa) {
-        return null;
+
+        return repositoryEmpresa.save(empresa);
     }
 
     @Override
     public void eliminarEmpresa(double nitEmpresa) {
 
+        repositoryEmpresa.deleteById(nitEmpresa);
     }
 }
