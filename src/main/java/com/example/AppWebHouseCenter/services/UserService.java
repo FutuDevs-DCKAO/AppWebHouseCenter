55247f885cf9cb35f2dfc86dcdbeb11dc9d5b63f
dianisr2 <dianis3008@gmail.com>
2022-09-04 20:59:29 -0500

    Actualización Servicios de Usuarios

--- src/main/java/com/example/AppWebHouseCenter/services/UserService.java
+++ src/main/java/com/example/AppWebHouseCenter/services/UserService.java
@@ -9,8 +9,8 @@ public interface UserService {
 
     public Empleado crearEmpleados(Empleado empleado); //Crea un usuario
     public Empleado consultaEmpleadoporId(String documento); //Consulta un empleado por documento
-    public Empleado editarEmpleado(Empleado empleado); //Edita un usuario
-    public void eliminarEmpleado(String documento); //Elimina un usuario
+    public Empleado editarEmpleado(Empleado empleado); //Edita un usuario existente
+    public void eliminarEmpleado(String documento); //Elimina un usuario existente
 
 
 }
