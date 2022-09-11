9e09748910c4fa95aacb5b8c7869b790ce79e575
CamiloSilva21 <juancamilosilva1@gmail.com>
2022-09-10 19:49:57 -0500

    Finalización y pruebas de los servicios y controladores de cada entidad

--- src/main/java/com/example/AppWebHouseCenter/repositories/RepositoryMovimientoDinero.java
+++ src/main/java/com/example/AppWebHouseCenter/repositories/RepositoryMovimientoDinero.java
@@ -1,6 +1,7 @@
 package com.example.AppWebHouseCenter.repositories;
 
 import com.example.AppWebHouseCenter.entities.MovimientoDinero;
+import com.example.AppWebHouseCenter.entities.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositoryMovimientoDinero extends JpaRepository<MovimientoDinero, String> {
