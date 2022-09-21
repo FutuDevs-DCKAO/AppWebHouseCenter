4ef88c1495dfd26561d6081091f3a915982942b5
CamiloSilva21 <juancamilosilva1@gmail.com>
2022-09-21 10:13:39 -0500

    Montaje exitoso de interfaz para consultar los movimientos de dinero

--- src/main/java/com/example/AppWebHouseCenter/controllers/MovimientoDineroController.java
+++ src/main/java/com/example/AppWebHouseCenter/controllers/MovimientoDineroController.java
@@ -6,30 +6,35 @@ import com.example.AppWebHouseCenter.entities.MovimientoDinero;
 import com.example.AppWebHouseCenter.services.EmpresaService;
 import com.example.AppWebHouseCenter.services.ImpMovimientoDineroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@RequestMapping("/enterprises")
-@RestController
+//@RequestMapping("/enterprises")
+//@RestController
+@Controller
 public class MovimientoDineroController {
 
     @Autowired
     private ImpMovimientoDineroService impMovimientoDineroService;
+
+    //Método que permite mostrar todos los movimientos de dinero en la tabla de los movimientos
+    @GetMapping("/enterprises/movements/all") //Permite listar todos los movimientos de dinero de una empresa
+    public String listarMovimientos(Model model){
+        model.addAttribute("movimientosDinero",this.impMovimientoDineroService.listarMovimientos());
+        return ("tableMovimientosDinero");
+    }
 
     @PostMapping("/movements")
     public MovimientoDinero insertarNuevosMovimientos(@RequestBody MovimientoDinero movimientoDinero){
         return impMovimientoDineroService.crearMovimientoDinero(movimientoDinero);
     }
 
-    @GetMapping("/movements/all") //Permite listar todos los movimientos de dinero de una empresa
-    public List<MovimientoDinero> listarMovimientos(){
-        return impMovimientoDineroService.listarMovimientos();
-    }
-
     @GetMapping("/{idMovimiento}/movements")
-    public MovimientoDinero consultaEmpleadoporId(@PathVariable("idMovimiento") String idMovimiento){
+    public MovimientoDinero consultaMovimientoporId(@PathVariable("idMovimiento") String idMovimiento){
         return impMovimientoDineroService.consultarMovimientoDineroporId(idMovimiento);
     }
     @PatchMapping("/{idMovimiento}/movements")
