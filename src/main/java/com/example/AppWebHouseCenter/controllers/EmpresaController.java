311861b7175f5218d0503761b115ce2e3f46e582
CamiloSilva21 <juancamilosilva1@gmail.com>
2022-09-26 17:42:02 -0500

    Funcionamiento form empresas, versiones de prueba para el registro de empleados y actualizaciones de todas las entidades

--- src/main/java/com/example/AppWebHouseCenter/controllers/EmpresaController.java
+++ src/main/java/com/example/AppWebHouseCenter/controllers/EmpresaController.java
@@ -4,10 +4,12 @@ import com.example.AppWebHouseCenter.entities.Usuario;
 import com.example.AppWebHouseCenter.services.ImpAdmUserService;
 import com.example.AppWebHouseCenter.services.ImpEmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,30 +24,13 @@ public class EmpresaController {
     //Creación objeto clase ImpEmpresaService para poder acceder a los métodos de la clase
     @Autowired
     private ImpEmpresaService impEmpresaService;
-    //@Autowired
 
 
-   /* @GetMapping("/")
-    public String inicio(Model model, @AuthenticationPrincipal OidcUser principal) {
-        if(principal !=null) {
-            System.out.print(principal.getClaims());
-        }
-        return "index";
-    }*/
-
-    //Response GET a través del objeto creado para obtener todas las empresas
-        //Método para poder obtener todas las empresas registradas en el sistema
+    //Método para poder obtener todas las empresas registradas en el sistema
     @GetMapping("/enterprises")
     public String listarEmpresas(Model model){
         model.addAttribute("empresas", this.impEmpresaService.listarEmpresas());
         return ("tableEmpresas");
-    }
-
-    //Response POST a través del objeto creado para crear nuevas empresas
-    @PostMapping("/enterprises/crear")
-    public Empresa crearEmpresa(@RequestBody Empresa empresa){
-
-        return impEmpresaService.crearEmpresa(empresa);
     }
 
     //Responde GET  a través del objeto creado para obtener una empresa mediante su primary key (NIT)
@@ -53,6 +38,24 @@ public class EmpresaController {
     public Empresa consultarEmpresa(@PathVariable("nitEmpresa") String nitEmpresa){
         return impEmpresaService.consultarEmpresaPorId(nitEmpresa);
     }
+
+    //------------------------------------------------------------------
+
+    //Método para mostrar el formulario de registro para una nueva empresa
+    @GetMapping("enterprises/register")
+    public String registroNuevaEmpresa(Model model){
+        model.addAttribute("empresainsertar", new Empresa());
+        return "formEmpresa";
+    }
+
+    //Response POST a través del objeto creado para crear nuevas empresas
+    @PostMapping("/enterprises/save")
+    public String crearEmpresa(@Validated Empresa empresa){
+        impEmpresaService.crearEmpresa(empresa);
+        return "redirect:/enterprises";
+    }
+
+
 
     //Response PUT para actualizar los datos de una empresa a través de la primary key (NIT)
     @PatchMapping("/{nitEmpresa}")
--- src/main/java/com/example/AppWebHouseCenter/controllers/UserControllers.java
+++ src/main/java/com/example/AppWebHouseCenter/controllers/UserControllers.java
@@ -5,6 +5,7 @@ import com.example.AppWebHouseCenter.services.ImpUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,21 +18,35 @@ public class UserControllers {
     @Autowired
     private ImpUserService impUserService;
 
+    //Método para obtener todos los empleados en una lista
     @GetMapping("/users")
     public String listarempleados(Model model){
         model.addAttribute("empleados", this.impUserService.listarempleados());
         return ("tableUsuarios");
     }
 
-    @PostMapping
-    public Empleado crearEmpleados(@RequestBody Empleado empleado){
-        return impUserService.crearEmpleados(empleado);
-    }
-
+    //Método para obtener el empleado por medio de su documento
     @GetMapping("/{documento}")
     public Empleado consultaEmpleadoporId(@PathVariable("documento") String documento){
         return impUserService.consultaEmpleadoporId(documento);
     }
+
+    //-------------------------------------------------------------------
+
+    //Método para mostrar el formulario de registro de los nuevos empleados a insertar
+    @GetMapping("users/register")
+    public String registrarEmpleado(Model model){
+        model.addAttribute("empleadoinsertar", new Empleado());
+        return ("formEmpleados");
+    }
+    //Método para redireccionar al sitio cuando el nuevo empleado haya sido registrado
+    @PostMapping("users/save")
+    public String crearEmpleados(@Validated Empleado empleado){
+        impUserService.crearEmpleados(empleado);
+        return "redirect:/users";
+    }
+
+    //----------------------------------------------
 
     @PatchMapping("/{documento}")
     public Empleado editarEmpleado(@PathVariable("documento") String documento, @RequestBody Map<Object, Object> objectMap){
